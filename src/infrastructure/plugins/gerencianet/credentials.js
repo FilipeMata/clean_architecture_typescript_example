6cbf702f10af4e7e235efb3b205976b7622c525d
FilipeMata <filipeemsantos92@gmail.com>
2022-01-02 14:15:34 -0300

    fix: resolve merge conflicts

--- src/infrastructure/plugins/gerencianet/credentials.js
+++ src/infrastructure/plugins/gerencianet/credentials.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-	client_id: '',
-    client_secret: '',
+	client_id: 'Client_Id_d329359f89a409ba4ec7297da75774e98b386029',
+    client_secret: 'Client_Secret_e1b1f79c8ab84a9c63e56dba9af915521d57ad29',
     sandbox: true
 }
