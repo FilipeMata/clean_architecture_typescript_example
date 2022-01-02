71154e3bff0da09c2ac6aaac68f711fd46828924
FilipeMata <filipeemsantos92@gmail.com>
2022-01-02 14:23:45 -0300

    fix: remove useless logs

--- src/use-cases/generate-order/generate-order.interactor.ts
+++ src/use-cases/generate-order/generate-order.interactor.ts
@@ -43,7 +43,6 @@ export default class GenerateOrderInteractor extends Interactor<GenerateOrderReq
     if (!billingAddress) {
       throw new ApplicationError('missing_order_billing_address');
     }
-    console.log('CHEGOU AQUI TA');
 
     const order = Order.build({
       billingAddress: billingAddress,
